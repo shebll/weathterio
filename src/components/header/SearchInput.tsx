3f9e5d9672504b8d8fcb0f1d0574ac34f07cc6be
shebll <shebblloll@gmail.com>
2024-06-28 04:18:00 +0300

    feat(header): Get current location
    - Get current location auto

--- src/components/header/SearchInput.tsx
+++ src/components/header/SearchInput.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef, useState } from "react";
-import { fetchCity } from "../../lib/api/cities";
+import { fetchCity, fetchCityByCoordinates } from "../../lib/api/cities";
 import { useCity } from "../../context/CityContext";
 
 export default function SearchInput() {
@@ -74,8 +74,19 @@ export default function SearchInput() {
     setShowPopup(false);
   };
 
+  const getCurrentLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(async (position) => {
+        const { latitude, longitude } = position.coords;
+        const cityData = await fetchCityByCoordinates(latitude, longitude);
+        if (cityData) {
+          setSelectedCity(cityData);
+        }
+      });
+    }
+  };
   return (
-    <div className="relative">
+    <div className="relative flex items-center gap-1">
       <input
         ref={searchInput}
         id="cityName"
@@ -86,6 +97,29 @@ export default function SearchInput() {
         type="text"
         className="border rounded-md bg-gray-100 outline-[0.5px] outline-gray-200 py-2 px-4"
       />
+      <div className="relative inline-block">
+        <button onClick={getCurrentLocation} className="relative z-10 group">
+          <img
+            src="/location-light.png"
+            alt="location png"
+            width={30}
+            height={30}
+            className="rounded-full bg-gray-200 p-2"
+          />
+        </button>
+        <div className="absolute right-[-200%] bottom-full mb-2 block bg-black/85 text-white text-sm rounded-md px-2 py-1 w-max">
+          Use Current Location
+          <svg
+            className="absolute text-gray-900 h-2 w-full left-0 top-full"
+            x="0px"
+            y="0px"
+            viewBox="0 0 255 255"
+            xmlSpace="preserve"
+          >
+            <polygon className="fill-current" points="0,0 127.5,127.5 255,0" />
+          </svg>
+        </div>
+      </div>
       {showPopup && (
         <div className="absolute max-h-64 overflow-auto rounded-md top-[104%] left-0 w-full bg-gray-100 flex flex-col gap-2">
           {loading ? (
--- src/lib/api/cities.ts
+++ src/lib/api/cities.ts
@@ -14,3 +14,22 @@ export async function fetchCity(cityName: string) {
   const data = await response.data;
   return data;
 }
+
+export async function fetchCityByCoordinates(
+  latitude: number,
+  longitude: number
+) {
+  const response = await axios.get(
+    `https://api.api-ninjas.com/v1/city?min_lat=${latitude - 0.1}&max_lat=${
+      latitude + 0.1
+    }&min_lon=${longitude - 0.1}&max_lon=${longitude + 0.1}&limit=1`,
+    {
+      headers: {
+        "X-Api-Key": `${import.meta.env.VITE_CITIES_API_KEY}`,
+        "Content-Type": "application/json",
+      },
+    }
+  );
+
+  return response.data.length > 0 ? response.data[0] : null;
+}
