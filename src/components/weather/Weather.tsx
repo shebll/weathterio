8adebf8414208b99cf74b8c8636d5e58e91a7c19
shebll <shebblloll@gmail.com>
2024-06-29 18:35:05 +0300

    refactor(weather): Reorganize weather component

--- src/components/weather/Weather.tsx
+++ src/components/weather/Weather.tsx
@@ -1,38 +1,14 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useCity } from "../../context/CityContext";
 
-import { fetchWeather } from "../../lib/api/weather";
-import type { WeatherType } from "../../type/Weather";
-
-import LocationAndDate from "./components/LocationAndDate";
-import WeatherIcon from "./components/WeatherIcon";
-import WeatherOverView from "./components/WeatherOverView";
-import WeatherInfo from "./components/WeatherInfo";
 import HourlyForecast from "./components/HourlyForecast";
 
 import { LoadingSkelton } from "./components/LoadingSkelton";
+import CurrentWeatherWidget from "./components/CurrentWeatherWidget";
 
 export default function Weather() {
   const { selectedCity } = useCity();
-
-  const [data, setData] = useState<WeatherType | null>(null);
-  const [loading, setLoading] = useState(false);
-
   const [tempType, setTempType] = useState<"c" | "f">("c");
-
-  async function fetchData(selectedCity: City) {
-    setLoading(true);
-    const weatherData = await fetchWeather(
-      selectedCity.latitude.toString(),
-      selectedCity.longitude.toString()
-    );
-    setData(weatherData);
-    setLoading(false);
-  }
-
-  useEffect(() => {
-    if (selectedCity) fetchData(selectedCity);
-  }, [selectedCity]);
 
   if (!selectedCity) {
     return (
@@ -44,38 +20,22 @@ export default function Weather() {
       </>
     );
   }
-  if (!data) return null;
 
-  if (loading) return <LoadingSkelton />;
+  return (
+    <div className="flex flex-col items-center w-full gap-10">
+      {/* Current Weather component */}
+      <CurrentWeatherWidget
+        selectedCity={selectedCity}
+        setTempType={setTempType}
+        tempType={tempType}
+      />
 
-  if (data)
-    return (
-      <div className="flex flex-col items-center w-full gap-10">
-        {/* location date component */}
-        <LocationAndDate
-          Timezone={data.timezone}
-          cityName={data.name}
-          country={data.sys.country}
-        />
-        {/* Weather Icon component */}
-        <WeatherIcon Weather={data.weather[0].main} Timezone={data.timezone} />
-
-        {/* Weather OverView component */}
-        <WeatherOverView
-          tempType={tempType}
-          setTempType={setTempType}
-          data={data}
-        />
-
-        {/* Weather Info for day component */}
-        <WeatherInfo tempType={tempType} data={data} />
-
-        {/* Forecast for every hours in day component */}
-        <HourlyForecast
-          tempType={tempType}
-          selectedCity={selectedCity}
-          timezone={data.timezone}
-        />
-      </div>
-    );
+      {/* Forecast for every hours in day component */}
+      <HourlyForecast
+        tempType={tempType}
+        selectedCity={selectedCity}
+        // timezone={data.timezone}
+      />
+    </div>
+  );
 }
--- src/components/weather/components/WeatherInfo.tsx
+++ src/components/weather/components/WeatherInfo.tsx
@@ -8,7 +8,7 @@ type props = {
 export default function WeatherInfo({ data, tempType }: props) {
   return (
     <div className="flex flex-row items-center w-full gap-20 p-4 overflow-y-auto bg-gray-200 rounded-md shadow-md">
-      <p className="flex items-center gap-3 text-xl font-medium w-max shrink-0">
+      <div className="flex items-center gap-3 text-xl font-medium w-max shrink-0">
         <img
           src="/temperature.png"
           alt="temperature image"
@@ -16,16 +16,16 @@ export default function WeatherInfo({ data, tempType }: props) {
           height={20}
         />{" "}
         <p>{getTempFromType(data.main.temp, tempType)}</p>
-      </p>
-      <p className="flex items-center gap-3 text-xl font-medium w-max shrink-0">
+      </div>
+      <div className="flex items-center gap-3 text-xl font-medium w-max shrink-0">
         <img src="/humidity.png" alt="humidity image" width={25} height={25} />
         {data.main.humidity} %
-      </p>
-      <p className="flex items-center gap-3 text-xl font-medium w-max shrink-0">
+      </div>
+      <div className="flex items-center gap-3 text-xl font-medium w-max shrink-0">
         <img src="/wind.png" alt="wind image" width={40} height={40} />{" "}
         <p>{data.wind.speed} KpH</p>
-      </p>
-      <p className="flex items-center gap-3 text-xl font-medium w-max shrink-0">
+      </div>
+      <div className="flex items-center gap-3 text-xl font-medium w-max shrink-0">
         <img
           src="/cloudiness.png"
           alt="cloudiness image"
@@ -33,7 +33,7 @@ export default function WeatherInfo({ data, tempType }: props) {
           height={40}
         />{" "}
         <p>{data.clouds.all} %</p>
-      </p>
+      </div>
     </div>
   );
 }
